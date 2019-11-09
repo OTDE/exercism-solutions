9a1a4693377caeb18d7ccf8f7c96f626a16fc138
OTDE <mail@sethchapman.com>
2019-11-09 12:52:34 -0800

    Added Allergies solution.

---
+++ java/allergies/src/main/java/Allergies.java
@@ -0,0 +1,31 @@
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Allergies {
+    private List<Allergen> allergens;
+
+    public Allergies(int allergenScore) {
+        allergens = IntStream
+                .range(0, Allergen.values().length)
+                .filter(offset -> isAllergenScoreBitSetAtOffset(allergenScore, offset))
+                .mapToObj(Allergies::toAllergen)
+                .collect(Collectors.toList());
+    }
+
+    private static boolean isAllergenScoreBitSetAtOffset(int allergenScore, int offset) {
+        return ((allergenScore >> offset) & 1) == 1;
+    }
+
+    private static Allergen toAllergen(int index) {
+        return Allergen.values()[index];
+    }
+
+    boolean isAllergicTo(Allergen allergen) {
+        return allergens.contains(allergen);
+    }
+
+    List<Allergen> getList() {
+        return allergens;
+    }
+}
